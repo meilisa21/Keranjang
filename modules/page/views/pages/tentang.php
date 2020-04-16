33521e2f76f46f9ba4fc287b04c09229bf2c4dbb
meilisa21 <62790692+meilisa21@users.noreply.github.com>
2020-04-16 15:04:37 +0700

    Add files via upload

---
+++ modules/page/views/pages/tentang.php
@@ -0,0 +1,5 @@
+<h2>Tentang Hits'Pizza</h2>
+<h3></h3>
+<p></p>
+<h3></h3>
+<p></p>
---
+++ modules/page/views/shopping/sukses.php
@@ -0,0 +1,5 @@
+<h2>Proses Order sukses</h2>
+<div class="kotak2">
+<p>Terima kasih sudah memesan makanan di Hits' Pizza Order anda akan kami proses, Silahkan tunggu 30 menit pesanan anda akan segera sampai ke tempat anda.<br>
+Jangan segan mengontak kami jika ada permasalahan!</p>
+</div>
